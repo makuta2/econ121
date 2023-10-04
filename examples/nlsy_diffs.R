0e09e50c2aef7eb85280bd614d307f686d5ba1a0
tvogl <tvogl@ucsd.edu>
2023-10-04 12:50:00 -0700

    .

--- examples/nlsy_diffs.R
+++ examples/nlsy_diffs.R
@@ -85,7 +85,7 @@ nlsy79_workers %>%
             sd=sd(laborinc18),
             n=n())
 
-# still an $19k difference
+# still a $19k difference
 
 # now let's look at log earnings.
 nlsy79_workers <- 
