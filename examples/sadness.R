2c20b7d629a4448b64021f8a02b3570d26b55aaa
tvogl <tvogl@ucsd.edu>
2023-10-25 16:09:56 -0700

    .

--- examples/sadness.R
+++ examples/sadness.R
@@ -20,7 +20,38 @@ nhis2010 <- nhis2010 %>% drop_na(asad)
 # generate a variable that equals one if ever sad, zero otherwise.
 table(nhis2010$asad)
 nhis2010$anysad <- ifelse(nhis2010$asad != "None of the time", 1, 0)
+table(nhis2010$anysad)
 # could have also used: nhis2010 <- nhis2010 %>% mutate(anysad = ifelse(asad > 0, 1, 0))
+# or: nhis2010 <- mutate(nhis2010, anysad = ifelse(asad > 0, 1, 0))
+
+# SOME DESCRIPTIVE GRAPHS
+
+# first, sadness by education. I will draw the markers proportional
+# to the number of observations in each cell to emphasize that there
+# are very few individuals with less than 10 years of education.
+nhis2010 %>%
+  drop_na(edyrs) %>%
+  group_by(edyrs) %>%
+  summarize(mean_anysad = mean(anysad), # mean of anysad within each education level
+            numobs = n()) %>%
+  ggplot(aes(x = edyrs, y = mean_anysad, size=numobs)) +
+    geom_point() # scatter plot
+
+# next, sadness by age and sex. here, I won't keep track of cell size.
+nhis2010 %>%
+  drop_na(age, male) %>%
+  group_by(age, male) %>%
+  summarize(mean_anysad = mean(anysad)) %>%
+  ggplot(aes(x = age, y = mean_anysad, color=factor(male))) + # I use factor() so the color scale is categorical, not continuous
+    geom_line() # line plots
+
+# finally, sadness by marital status
+nhis2010 %>%
+  drop_na(marstat) %>%
+  group_by(marstat) %>%
+  summarize(mean_anysad = mean(anysad)) %>%
+  ggplot(aes(x = marstat, y = mean_anysad)) + 
+    geom_bar(stat = "identity") # bar plot. identity just tells it to plot the y we specified, instead of a count or proportion.
 
 # COMPARING OLS, LOGIT, AND PROBIT
 
